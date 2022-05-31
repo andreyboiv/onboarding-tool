8506dbdb79dff5b5c1882888144c358493834ff3
Andrey Boivalenko <andrey.boiv@gmail.com>
2022-05-31 21:39:47 +0200

    Initial commit
    Spring Boot Empty Projekt
    Spring Boot Version: 2.7.0
    Dependencies:
    -Spring Configuration Processor
    -Spring Boot DevTools
    -Spring Web
    -Spring Data JPA
    -Spring Security
    -PostgreSQL Driver
    -Lombok

---
+++ src/main/java/com/boivalenko/businessapp/web/TeamtasksplanningApplication.java
@@ -0,0 +1,13 @@
+package com.boivalenko.businessapp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtasksplanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamtasksplanningApplication.class, args);
+    }
+
+}
